a16f7e7ab0a40844d1ed88d37dae1bef745a0b58
Saswata Arabinda <sasarabi@WKMZT4583397.global.publicisgroupe.net>
2023-04-26 11:26:49 +0530

    Fuelgrowth | Beautify sidebar

--- fuelgrowth/src/components/SessionRow.jsx
+++ fuelgrowth/src/components/SessionRow.jsx
@@ -6,7 +6,7 @@ import { TrashIcon } from "@heroicons/react/24/solid";
 import React, { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { generatePath, Link, useParams } from "react-router-dom";
+import { generatePath, Link, useNavigate, useParams } from "react-router-dom";
 import { setError } from "../utils/errors";
 
 export const SessionRow = ({
@@ -17,19 +17,25 @@ export const SessionRow = ({
   updated,
 }) => {
   const [isActive, setActive] = useState(false);
+  const [loader, setLoader] = useState(false);
   const { sessionId } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { CHAT_PAGE } = ROUTES;
+  const { CHAT_PAGE, DASHBOARD } = ROUTES;
   const handleDelete = async (chatSessionId, e) => {
     e.preventDefault();
+    setLoader(true);
+
     try {
       const result = await deleteChatSession(chatSessionId);
-
       dispatch(removeSession(chatSessionId));
+      // Redirect to dashboard
+      navigate(DASHBOARD);
     } catch (error) {
       setError(error);
     }
+    setLoader(false);
   };
 
   useEffect(() => {
@@ -44,26 +50,80 @@ export const SessionRow = ({
         to={generatePath(CHAT_PAGE, {
           sessionId: chat_session_id,
         })}
-        className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
-          isActive && "bg-gray-300 50"
-        }`}
+        // className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
+        //   isActive && "bg-gray-300 50"
+        // }`}
+        className={`flex p-2 items-center text-sm gap-3 relative rounded-md cursor-pointer break-all text-gray-900  group ${
+          isActive && "bg-gray-300 pr-7"
+        } ${!isActive && "hover:bg-gray-100"}`}
       >
-        {/* <a href="#"> */}
-        <ChatBubbleLeftIcon className="h-4 w-4" />
-        <span
-          className="flex-1 ml-3 whitespace-nowrap truncate"
+        {/* <ChatBubbleLeftIcon className="h-4 w-4" /> */}
+        <svg
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="h-4 w-4"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+        </svg>
+        <div
+          className="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative"
           title={summary}
         >
           {summary}
-        </span>
-        {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                        3
-                      </span> */}
-        <TrashIcon
-          className="h-4 w-4"
-          onClick={(e) => handleDelete(chat_session_id, e)}
-        />
-        {/* </a> */}
+          {isActive && (
+            <div className="absolute inset-y-0 right-0 w-10 z-10 bg-gradient-to-l from-gray-300 "></div>
+          )}
+        </div>
+        {isActive && (
+          <div className="absolute flex right-1 z-10 text-gray-700/50 visible">
+            {/* <button className="p-1 hover:text-white">
+              <svg
+                stroke="currentColor"
+                fill="none"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="h-4 w-4"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M12 20h9"></path>
+                <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+              </svg>
+            </button> */}
+            <button
+              className="p-1 hover:text-black"
+              onClick={(e) => handleDelete(chat_session_id, e)}
+            >
+              <svg
+                stroke="currentColor"
+                fill="none"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="h-4 w-4"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <polyline points="3 6 5 6 21 6"></polyline>
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                <line x1="10" y1="11" x2="10" y2="17"></line>
+                <line x1="14" y1="11" x2="14" y2="17"></line>
+              </svg>
+            </button>
+          </div>
+        )}
       </Link>
     </li>
   );
